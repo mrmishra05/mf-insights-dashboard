33b22a417b853904a52d629a6a5ed828b96f91de
mrmishra05 <manish.agilesuccess@gmail.com>
2025-07-04 13:24:16 +0530

    Create top_stocks.py

---
+++ scraper/top_stocks.py
@@ -0,0 +1,13 @@
+def get_top_10_smallcaps():
+    return [
+        "KPI Green Energy", "Jindal Poly Films", "Borosil Renewables",
+        "Tanla Platforms", "Ujjivan Small Finance Bank", "Fine Organic",
+        "Deepak Fertilizers", "Mazagon Dock", "Multibase India", "Venus Pipes"
+    ]
+
+def get_top_10_midcaps():
+    return [
+        "PI Industries", "Tube Investments", "Suprajit Engineering",
+        "Cholamandalam Investment", "Polycab", "Balkrishna Industries",
+        "Apollo Tyres", "AU Small Finance Bank", "Persistent Systems", "Can Fin Homes"
+    ]
